959e1ebe9fe2255a473c0f1a1c8e543cf714f10d
hemuuolo <hemant.girase@uolo.com>
2024-11-27 14:30:07 +0530

    intial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    
+    return (
+      <div>
+        <h>Hemant girase</h>
+
+      </div>
+    );
+  }
+}
