0340630207ad16fd3fa80a5da918366d89231914
hemuuolo <hemant.girase@uolo.com>
2024-12-04 00:24:35 +0530

    minor changes in navbar

--- src/component/Navbar/navbar.js
+++ src/component/Navbar/navbar.js
@@ -2,13 +2,13 @@ import './navbar.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function Navbar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div>
       < nav className="navbar bg-primary" data-bs-theme="dark">
       
   <div className="container-fluid">
-    <a className="navbar-brand">Task</a>
+    <p className="navbar-brand">Task</p>
     <form className="d-flex" role="search">
     <div className="d-grid gap-2 d-md-flex justify-content-md-end">
      <button className="btn btn-primary me-md-2" type="button" onClick={() => navigate('/')}>Login</button>
